81b93db283ab6edf94623fd35dac05bc9f1e0b52
karsanidze <54028850+karsanidze@users.noreply.github.com>
2024-03-01 18:08:08 +0400

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -1,50 +1,60 @@
 import random
 
+
+class Hangman:
+    def __init__(self, words):
+        self.words = words
+        self.secret_word = random.choice(self.words)
+        self.guesses = []
+        self.max_attempts = 10
+        self.attempts_left = self.max_attempts
+
+    def display_word(self):
+        displayed_word = ""
+        for letter in self.secret_word:
+            if letter in self.guesses:
+                displayed_word += letter
+            else:
+                displayed_word += "_"
+        return displayed_word
+
+    def guess_letter(self, letter):
+        self.guesses.append(letter)
+        if letter not in self.secret_word:
+            self.attempts_left -= 1
+
+    def guess_word(self, word):
+        if word.lower() == self.secret_word:
+            self.guesses.extend(word.lower())
+        else:
+            self.attempts_left -= 1
+
+    def is_game_over(self):
+        if self.attempts_left <= 0:
+            return True, "\nSorry, you can't try anymore. The word was: " + self.secret_word
+        elif '_' not in self.display_word():
+            return True, "\nCongratulations! The word is: " + self.secret_word
+        return False, ""
+
+
 def main():
-    word = choose_word()
-    guessed_letters = []
-    tries = 6
+    words = ["step", "gita", "python", "hangman", "exam", "grade",
+             "django", "semester", "family", "name", "country"]
+    game = Hangman(words)
 
-    while True:
-        print("\nAttempts left:", tries)
-        print("Word:", display_word(word, guessed_letters))
+    print("Guess the word:", game.display_word())
 
-        guess = input("Guess a letter: ").lower()
-
-        if guess in guessed_letters:
-            print("You already guessed that letter.")
-        elif guess in word:
-            guessed_letters.append(guess)
-            print("Correct!")
+    while not game.is_game_over()[0]:
+        guess = input("\nGuess a letter: ").lower()
+        if len(guess) == 1:
+            game.guess_letter(guess)
         else:
-            tries -= 1
-            print("Incorrect!")
+            game.guess_word(guess)
+        print("\nWord:", game.display_word())
+        print("Attempts left:", game.attempts_left)
 
-        if guess == word:
-            print("\nCongratulations! The word is:", word)
-            break
-    
-        if "_" not in display_word(word, guessed_letters):
-            print("\nCongratulations! The word is:", word)
-            break
-        elif tries == 0:
-            print("\nSorry, you can't try anymore. The word was:", word)
-            break
-        
-
-def choose_word():
-    word_list = ["step", "gita", "python", "hangman", "exam", "grade", "django", "semester", "family", "name", "country"]
-    return random.choice(word_list)
+    print(game.is_game_over()[1])
 
 
-def display_word(word, guessed_letters):
-    display = ""
-    for letter in word:
-        if letter in guessed_letters:
-            display += letter
-        else:
-            display += "_"
-    return display
-
-
-main()
+if __name__ == "__main__":
+    main()
