c7962b04dec23ce41dd753b0c6d34829f08955c1
karsanidze <54028850+karsanidze@users.noreply.github.com>
2024-03-01 18:09:32 +0400

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -33,11 +33,15 @@ while True:
 
         if choice == '1':
             print("Result:", add(num1, num2))
+            break
         elif choice == '2':
             print("Result:", subtract(num1, num2))
+            break
         elif choice == '3':
             print("Result:", multiply(num1, num2))
+            break
         elif choice == '4':
             print("Result:", divide(num1, num2))
+            break
     else:
         print("Invalid Input")
